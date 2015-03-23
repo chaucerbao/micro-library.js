7e8eb5a8cb7ffe0958c0c198f53590881a72621a
Alvin Chan <aschan@gmail.com>
2015-03-23 14:59:34 -0700

    Add EnhancedElement#addClass()

--- src/model/EnhancedElement.js
+++ src/model/EnhancedElement.js
@@ -1,5 +1,22 @@
 export default class EnhancedElement {
   constructor(element) {
     this.element = element;
+    this.length = element ? element.length : null;
+  }
+
+  addClass(classes) {
+    this.each((element) => {
+      element.classList.add(...classes.split(' '));
+    });
+
+    return this;
+  }
+
+  each(callback) {
+    let elements = this.length ? this.element : [this.element];
+
+    for (let i = 0, length = elements.length; i < length; i++) {
+      callback(elements[i]);
+    }
   }
 }
---
+++ test/model/EnhancedElement.js
@@ -0,0 +1,45 @@
+System.import('model/EnhancedElement').then(function(EnhancedElement) {
+  var expect = chai.expect,
+    sandbox = document.getElementById('sandbox')
+
+  describe('EnhancedElement', function() {
+    beforeEach(function() {
+      sandbox.innerHTML = '\
+        <div id="id-1" class="class-A class-Z"></div>\
+        <div id="id-2" class="class-A class-Z"></div>\
+      ';
+    })
+
+    after(function() {
+      while (sandbox.lastChild) {
+        sandbox.removeChild(sandbox.lastChild)
+      }
+    })
+
+    describe('#addClass()', function() {
+      it('adds space-separated classes to an element', function() {
+        var single = new EnhancedElement.default(document.getElementById('id-1'));
+
+        expect(single.element.classList.contains('class-B')).to.be.false
+        expect(single.element.classList.contains('class-C')).to.be.false
+        single.addClass('class-B class-C')
+        expect(single.element.classList.contains('class-B')).to.be.true
+        expect(single.element.classList.contains('class-C')).to.be.true
+      })
+
+      it('adds space-separated classes to each element in a collection', function() {
+        var collection = new EnhancedElement.default(document.getElementsByClassName('class-A'));
+
+        expect(collection.element[0].classList.contains('class-B')).to.be.false
+        expect(collection.element[0].classList.contains('class-C')).to.be.false
+        expect(collection.element[1].classList.contains('class-B')).to.be.false
+        expect(collection.element[1].classList.contains('class-C')).to.be.false
+        collection.addClass('class-B class-C')
+        expect(collection.element[0].classList.contains('class-B')).to.be.true
+        expect(collection.element[0].classList.contains('class-C')).to.be.true
+        expect(collection.element[1].classList.contains('class-B')).to.be.true
+        expect(collection.element[1].classList.contains('class-C')).to.be.true
+      })
+    })
+  })
+})
